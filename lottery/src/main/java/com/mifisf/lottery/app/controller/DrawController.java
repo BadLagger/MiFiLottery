e132f8029724c37598e956f933b7e90dcb92f9b2
Vladimir Hrechko <grechko_vo@navis.ru>
2025-04-24 12:23:56 +0300

    Добавил GET /api/draws/{id}/results

--- lottery/src/main/java/com/mifisf/lottery/app/controller/DrawController.java
+++ lottery/src/main/java/com/mifisf/lottery/app/controller/DrawController.java
@@ -1,6 +1,7 @@
 package com.mifisf.lottery.app.controller;
 
 import com.mifisf.lottery.app.entity.Draw;
+import com.mifisf.lottery.app.entity.DrawResult;
 import com.mifisf.lottery.app.service.DrawService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,28 +13,33 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class DrawController {
-    public final DrawService service;
+    public final DrawService drawService;
 
-    public DrawController(DrawService service) {
-        this.service = service;
+    public DrawController(DrawService drawService) {
+        this.drawService = drawService;
     }
 
     @GetMapping("/draws/all")
-    public List<Draw> getAllDraws() { return service.findAll();}
+    public List<Draw> getAllDraws() { return drawService.findAll();}
 
     @GetMapping("/draws/active")
-    public List<Draw> getActiveDraws() { return service.findByStatus(Draw.DrawStatus.ACTIVE);}
+    public List<Draw> getActiveDraws() { return drawService.findByStatus(Draw.DrawStatus.ACTIVE);}
 
     /* Get Active or Planned draws */
     @GetMapping("/draws/available")
     public List<Draw> getAvailableDraws() {
-        var  result = service.findByStatus(Draw.DrawStatus.ACTIVE);
-        result.addAll(service.findByStatus(Draw.DrawStatus.PLANNED));
+        var  result = drawService.findByStatus(Draw.DrawStatus.ACTIVE);
+        result.addAll(drawService.findByStatus(Draw.DrawStatus.PLANNED));
         return result;
     }
 
     @GetMapping("/draws/{id}")
     public Draw getDrawsById(@PathVariable Long id) {
-        return service.findById(id).orElse(null);
+        return drawService.findById(id).orElse(null);
+    }
+
+    @GetMapping("/draws/{id}/results")
+    public DrawResult getDrawIdResults(@PathVariable Long id) {
+        return drawService.findResultByDrawId(id);
     }
 }
--- lottery/src/main/java/com/mifisf/lottery/app/service/DrawService.java
+++ lottery/src/main/java/com/mifisf/lottery/app/service/DrawService.java
@@ -1,7 +1,9 @@
 package com.mifisf.lottery.app.service;
 
 import com.mifisf.lottery.app.entity.Draw;
+import com.mifisf.lottery.app.entity.DrawResult;
 import com.mifisf.lottery.app.repository.DrawRepository;
+import com.mifisf.lottery.app.repository.DrawResultRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +13,20 @@ import java.util.Optional;
 @Service
 public class DrawService {
     @Autowired
-    private DrawRepository repository;
+    private DrawRepository drawRepository;
 
-    public List<Draw> findAll() {return repository.findAll();}
+    @Autowired
+    private DrawResultRepository drawResultRepository;
+
+    public List<Draw> findAll() {return drawRepository.findAll();}
 
     public List<Draw> findByStatus(Draw.DrawStatus status) {
-        return repository.findByStatus(status.toString());
+        return drawRepository.findByStatus(status.toString());
     }
 
-    public Optional<Draw> findById(Long id) { return repository.findById(id); }
+    public Optional<Draw> findById(Long id) { return drawRepository.findById(id); }
+
+    public DrawResult findResultByDrawId(Long id) {
+        return  drawResultRepository.findByDraw(drawRepository.findById(id).orElse(null));
+    }
 }
