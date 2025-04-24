dae04b3e4fe282dd0c0c7aa16bcfbfeb0acfcdbe
Kovalev Evgeny <mr.ekovalev@yandex.com>
2025-04-24 22:52:05 +0300

    kev:init

--- src/main/java/com/example/lottery/LotteryApplication.java
+++ src/main/java/com/example/lottery/LotteryApplication.java
@@ -1,18 +1,11 @@
 package com.example.lottery;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-
-//@SpringBootApplication
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-})
-//todo убрать потом exclude, так как он отключает подключение к бд
+@SpringBootApplication
 public class LotteryApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(LotteryApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(LotteryApplication.class, args);
+  }
 }
