db855e415cd277ababa5933c6c2a65d10dfb7ab0
Vladimir Hrechko <grechko_vo@navis.ru>
2025-04-24 14:58:02 +0300

    Удаление лишнего класса LotteryRepository

--- src/main/java/com/example/lottery/repository/LotteryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.lottery.repository;
-
-import com.example.lottery.entity.LotteryType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LotteryRepository extends JpaRepository<LotteryType, Long> {
-}
