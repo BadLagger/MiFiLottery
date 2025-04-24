a143e7483f27115ed79dfa69fde52162116ad1ee
BadLagger <lanynamer1985@gmail.com>
2025-04-25 00:04:10 +0300

    Добавил POST /api/admin/draws. (пока без проверки на дубли)

--- src/main/java/com/example/lottery/controller/DrawController.java
+++ src/main/java/com/example/lottery/controller/DrawController.java
@@ -1,20 +1,33 @@
 package com.example.lottery.controller;
 
+import com.example.lottery.dto.DrawRequestDto;
+import com.example.lottery.dto.DrawResultDto;
 import com.example.lottery.dto.DrawStatus;
 import com.example.lottery.entity.Draw;
 import com.example.lottery.entity.DrawResult;
 import com.example.lottery.service.DrawService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class DrawController {
     public final DrawService drawService;
+
+    @Value("${app.default.draw-duration}")
+    private Integer defaultDuration;
 
     public DrawController(DrawService drawService) {
         this.drawService = drawService;
@@ -43,4 +56,40 @@ public class DrawController {
     public DrawResult getDrawIdResults(@PathVariable Long id) {
         return drawService.findResultByDrawId(id);
     }
+
+    @PostMapping("/admin/draws")
+    public ResponseEntity<Draw> createDraw(@Valid @RequestBody DrawRequestDto request) throws MethodArgumentNotValidException {
+
+        if (request.duration() == null) {
+            request = new DrawRequestDto(
+                    request.name(),
+                    request.lotteryTypeId(),
+                    request.startTime(),
+                    defaultDuration);
+        } else {
+            if (request.duration() <= 0) {
+                throw new IllegalArgumentException("Duration should be positive");
+            }
+        }
+
+        Draw draw= drawService.createDraw(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(draw);
+    }
+
+    // Обработчик ошибок
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ResponseEntity<Object> handleIllegalArguments(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+    // Обработчик ошибок
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Object> handleValidationErrors(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        List<FieldError> fieldErrors = result.getFieldErrors();
+        List<String> errors = fieldErrors.stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        return ResponseEntity.badRequest().body(errors);
+    }
+
 }
--- src/main/java/com/example/lottery/service/DrawService.java
+++ src/main/java/com/example/lottery/service/DrawService.java
@@ -1,10 +1,16 @@
 package com.example.lottery.service;
 
+import com.example.lottery.dto.DrawRequestDto;
+import com.example.lottery.dto.DrawResultDto;
 import com.example.lottery.dto.DrawStatus;
 import com.example.lottery.entity.Draw;
 import com.example.lottery.entity.DrawResult;
+import com.example.lottery.entity.LotteryType;
+import com.example.lottery.mapper.DrawMapper;
 import com.example.lottery.repository.DrawRepository;
 import com.example.lottery.repository.DrawResultRepository;
+import com.example.lottery.repository.LotteryTypeRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +25,15 @@ public class DrawService {
     @Autowired
     private DrawResultRepository drawResultRepository;
 
+    @Autowired
+    private LotteryTypeRepository lotteryTypeRepository;
+
+    private final DrawMapper drawMapper;
+
+    public DrawService(DrawMapper drawMapper) {
+        this.drawMapper = drawMapper;
+    }
+
     public List<Draw> findAll() {return drawRepository.findAll();}
 
     public List<Draw> findByStatus(DrawStatus status) {
@@ -29,5 +44,11 @@ public class DrawService {
 
     public DrawResult findResultByDrawId(Long id) {
         return  drawResultRepository.findByDraw(drawRepository.findById(id).orElse(null));
+    }
+
+    public Draw createDraw(DrawRequestDto request) {
+        LotteryType lotteryType = lotteryTypeRepository.findById(request.lotteryTypeId()).orElseThrow(() -> new IllegalArgumentException("Type of lottery not found"));
+        Draw draw = drawMapper.toEntity(request, lotteryType, DrawStatus.PLANNED);
+        return drawRepository.save(draw);
     }
 }
