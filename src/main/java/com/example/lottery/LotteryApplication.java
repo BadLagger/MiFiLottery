23d3b47c39a8820f777060ef946c1af3bed6f33b
Vladimir Hrechko <grechko_vo@navis.ru>
2025-04-24 15:09:35 +0300

    Перенос основного класса LotteryApplication из отладочной ветки в рабочую

--- src/main/java/com/example/lottery/LotteryApplication.java
+++ src/main/java/com/example/lottery/LotteryApplication.java
@@ -1,18 +1,22 @@
 package com.example.lottery;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
-
-//@SpringBootApplication
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-})
-//todo убрать потом exclude, так как он отключает подключение к бд
+@SpringBootApplication
 public class LotteryApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(LotteryApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotteryApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Start APP !!!");
+		};
+	}
 }
