c06c77195e8e97f3ba123c718ba6a35bb0094a9a
Vladimir Hrechko <grechko_vo@navis.ru>
2025-04-24 15:17:11 +0300

    Перенос Draw из отладочной ветки в основную

--- src/main/java/com/example/lottery/entity/Draw.java
+++ src/main/java/com/example/lottery/entity/Draw.java
@@ -2,27 +2,43 @@ package com.example.lottery.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name="draw")
 public class Draw {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String lotteryType;
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="lottery_type_id")
+    private LotteryType lotteryType;
+
+    @Column(nullable = false)
     private LocalDateTime startTime;
 
-    @Enumerated(EnumType.STRING)
-    private Status status;
+    @Column(nullable = false)
+    private Integer duration;
 
-    public enum Status {
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "draw_status")
+    private DrawStatus status;
+
+    public enum DrawStatus {
+
         PLANNED, ACTIVE, COMPLETED, CANCELLED
+
     }
 }
