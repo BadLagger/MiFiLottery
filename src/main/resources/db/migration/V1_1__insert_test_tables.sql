3244a98da565b846a34dcecfb012374fc0b5146a
Vladimir Hrechko <grechko_vo@navis.ru>
2025-04-25 15:31:50 +0300

    Подтянул файлы миграции БД от Евгения

---
+++ src/main/resources/db/migration/V1_1__insert_test_tables.sql
@@ -0,0 +1,18 @@
+-- Начальные данные
+INSERT INTO LotteryType (description, ticket_price, min_ticket, prize_pool_percentage) VALUES
+    ('auto', 100.0, 5, 0.6);
+
+INSERT INTO Roles (name, description) VALUES
+                                          ('admin', 'Администратор'),
+                                          ('user', 'Пользователь');
+
+INSERT INTO Users (name, telegram, balance, role_id) VALUES
+                                                         ('User1', 'simple_user1', 0, 2),
+                                                         ('User2', 'simple_user2', 100, 2),
+                                                         ('Admin', 'iamadmin', 0, 1);
+
+INSERT INTO Draw(lottery_type_id, name, start_time, duration, status) VALUES
+                                                                          (1, 'Lottery1', TIMESTAMPADD(SQL_TSI_HOUR, 2, CURRENT_TIMESTAMP), 5, 'PLANNED'), -- добавляем 2 часа
+                                                                          (1, 'Lottery2', TIMESTAMPADD(SQL_TSI_HOUR, 10, CURRENT_TIMESTAMP), 7, 'PLANNED'), -- добавляем 10 часов
+                                                                          (1, 'Lottery3', TIMESTAMPADD(SQL_TSI_HOUR, -1, CURRENT_TIMESTAMP), 2, 'ACTIVE'), -- добавляем 1 час
+                                                                          (1, 'Lottery4', TIMESTAMPADD(SQL_TSI_HOUR, -3, CURRENT_TIMESTAMP), 3, 'COMPLETED'); -- вычитаем 3 часа
