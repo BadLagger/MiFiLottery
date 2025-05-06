853f77258886f6e27636998f07bde9d0580a91e8
Vladimir Hrechko <grechko_vo@navis.ru>
2025-05-06 14:07:34 +0300

    Перенёс обработчики исключений из security

--- src/main/java/com/example/lottery/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/lottery/exception/GlobalExceptionHandler.java
@@ -2,9 +2,15 @@ package com.example.lottery.exception;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.session.SessionAuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -57,5 +63,51 @@ public class GlobalExceptionHandler {
     return new ResponseEntity<>(
         new AppError(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()),
         HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<?> handleConstraintViolation(ConstraintViolationException e) {
+    Map<String, String> errors = new HashMap<>();
+    e.getConstraintViolations().forEach(v -> {
+      String field = v.getPropertyPath().toString();
+      String message = v.getMessage();
+      errors.put(field, message);
+    });
+    log.error(errors.toString(), e);
+    return ResponseEntity.badRequest().body(errors);
+  }
+
+
+  @ExceptionHandler(BadCredentialsException.class)
+  public ResponseEntity<String> handleBadCredentials(BadCredentialsException ex) {
+    return errorResponseWithStatus(
+            "Error: wrong login or password!",
+            HttpStatus.UNAUTHORIZED);
+  }
+
+  @ExceptionHandler(AccessDeniedException.class)
+  public ResponseEntity<String> handleAccessDenied(AccessDeniedException ex) {
+    return errorResponseWithStatus(
+            "Error: don't have enough permissions",
+            HttpStatus.UNAUTHORIZED);
+  }
+
+  @ExceptionHandler(SessionAuthenticationException.class)
+  public ResponseEntity<String> handleSessionAuthenticationException(SessionAuthenticationException ex) {
+    return errorResponseWithStatus(
+            "Error: This session already exists",
+            HttpStatus.FORBIDDEN);
+  }
+
+  @ExceptionHandler(UsernameNotFoundException.class)
+  public ResponseEntity<String> usernameNotFound(UsernameNotFoundException ex) {
+    return errorResponseWithStatus(
+            ex.getMessage(),
+            HttpStatus.NOT_FOUND);
+  }
+
+  private ResponseEntity<String> errorResponseWithStatus(String msg, HttpStatus status) {
+    log.error(msg);
+    return ResponseEntity.status(status).body(msg);
   }
 }
