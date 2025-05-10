efe8d8c385191b2b7cec1abdb0d25a9356edd334
BadLagger <lanynamer1985@gmail.com>
2025-05-10 14:41:08 +0300

    Убрал закомментированный код. Изменил тип возвращаемого значения getUserByName на Optional

--- src/main/java/com/example/lottery/service/UserService.java
+++ src/main/java/com/example/lottery/service/UserService.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Service;
 import com.example.lottery.repository.RoleRepository;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -91,74 +92,10 @@ public class UserService {
 
     }
 
-    public User getUserByName(String username) {
-        return userRepository.findByName(username).orElse(null);
+    public Optional<User> getUserByName(String username) {
+        return userRepository.findByName(username);
     }
 
-    // Будет возвращать User без проверки пароля
-    /*public User getUserByUsername(String username) {
-        return userRepository.findByUsername(username)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-    }*/
-
-    // Проверяет пароль (возвращает true/false вместо исключения)
-    /*public boolean checkPassword(String username, String rawPassword) {
-        User user = getUserByUsername(username);
-        return BCrypt.checkpw(rawPassword, user.getPassword());
-    }*/
-
-    /**
-     * Возвращает текущий баланс пользователя.
-     * @param userId ID пользователя
-     * @return Значение баланса
-     * @throws IllegalArgumentException Если пользователь не найден
-     */
-
-    /*public BigDecimal getBalance(Long userId) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
-             return user.getBalance();
-    }*/
-
-    /**
-     * Списывает средства с баланса.
-     * @param userId ID пользователя
-     * @param amount Сумма (должна быть > 0 и <= текущему балансу)
-     * @throws IllegalStateException Если недостаточно средств
-     */
-
-    /*public void subtractFromBalance(Long userId, BigDecimal amount) {
-        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
-            throw new IllegalArgumentException("Amount must be positive and not null");
-        }
-        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
-
-        if (user.getBalance() == null) {
-            throw new IllegalStateException("User balance is not initialized");
-        }
-
-        if (user.getBalance().compareTo(amount) < 0) {
-            throw new IllegalStateException("Insufficient balance");
-        }
-        user.setBalance(user.getBalance().subtract(amount));
-        userRepository.save(user);
-    }*/
-
-    /**
-     * Пополняет баланс пользователя.
-     * @param userId ID пользователя
-     * @param amount Сумма (должна быть > 0)
-     * @throws IllegalArgumentException Если сумма некорректна
-     */
-
-    /*public void addToBalance (Long userId, BigDecimal amount) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
-        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
-            throw new IllegalArgumentException("Amount must be positive");
-        }
-            BigDecimal newBalance = user.getBalance().add(amount);
-            user.setBalance(newBalance);
-            userRepository.save(user);
-        }*/
     }
 
 
